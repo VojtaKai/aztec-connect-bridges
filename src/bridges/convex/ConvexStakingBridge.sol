174b89f7fb10daa19b0ef0427738ee27f3055860
Vojtech Kaiser <vojtechkaiser95@gmail.com>
2022-09-24 00:43:14 +0200

    Staking and Unstaking with fuzzing ability, transfers of tokens from/to rollup processor. Asserts of balances of CSB tokens fro bridge and rollup processor, bridge refactoring

--- src/bridges/convex/ConvexStakingBridge.sol
+++ src/bridges/convex/ConvexStakingBridge.sol
@@ -2,6 +2,9 @@
 // Copyright 2022 Aztec.
 pragma solidity >=0.8.4;
 
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
 import {BridgeBase} from "../base/BridgeBase.sol";
 import {AztecTypes} from "../../aztec/libraries/AztecTypes.sol";
 import {ICurvePool} from "../../interfaces/curve/ICurvePool.sol";
@@ -12,25 +15,39 @@ import {IConvexToken} from "../../interfaces/convex/IConvexToken.sol";
 import {ICurveLpToken} from "../../interfaces/convex/ICurveLpToken.sol";
 import {ICurveRewards} from "../../interfaces/convex/ICurveRewards.sol";
 
-import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 /**
  * @notice A DefiBridge that stakes Curve LP Token into Convex Finance and receives Convex Token as an ouput 
  * @dev Synchronous and stateless bridge that will hold no funds beyond dust for gas-savings.
  * @author Vojtech Kaiser
  */
-contract ConvexStakingBridge is BridgeBase {
+contract ConvexStakingBridge is BridgeBase, ERC20("ConvexStakingBridge", "CSB") {
     using SafeERC20 for IConvexFinanceBooster;
     using SafeERC20 for IConvexToken;
     using SafeERC20 for ICurveLpToken;
     // using SafeERC20 for ICurveRewards; // not really needed...I am not sending anything, just checking balance
+    struct PoolInfo {
+        uint poolPid;
+        address curveLpToken;
+        address convexToken;
+        address curveRewards;
+        bool exists;
+    }
 
     address public immutable rollupProcessor;
-    uint public poolPid;
-    bool public isMatchingPoolFound;
+    // uint public poolPid;
+    
+
+
+    PoolInfo public poolinfo;
+
+    mapping(address => PoolInfo) public pools;
+    uint public lastPoolLength;
+
+    // bool public isMatchingPoolFound;
     // uint public constant PRECISION = 1e18;
 
-    // Deposit Contract for Convex Finance
+    // Deposit Contract for Convex Finance - Main File
     IConvexFinanceBooster public constant CONVEX_DEPOSIT = IConvexFinanceBooster(0xF403C135812408BFbE8713b5A23a04b3D48AAE31);
 
     // General Interface for any Convex Token Contract
@@ -40,28 +57,56 @@ contract ConvexStakingBridge is BridgeBase {
     ICurveLpToken public CURVE_LP_TOKEN;
 
     // General Interface for any Curve Rewards Contract
-    ICurveRewards public CURVE_REWARDS = ICurveRewards(0x14F02f3b47B407A7a0cdb9292AA077Ce9E124803); // NENI DYNAMICKY, HAZI ERROR!!!
+    // ICurveRewards public CURVE_REWARDS = ICurveRewards(0x14F02f3b47B407A7a0cdb9292AA077Ce9E124803); // NENI DYNAMICKY, HAZI ERROR!!!
+    ICurveRewards public CURVE_REWARDS; // NENI DYNAMICKY, HAZI ERROR!!!
     
     error stakingUnsuccessful();
     error withdrawalUnsuccessful();
     error invalidTotalInputValue();
+    error invalidInputOutputAssets();
+    error poolIsClosed();
 
     event StakingResult(bool isStakingSuccessful);
+    event BridgeTokenAmount(uint tokenAmount);
 
     
     constructor(address _rollupProcessor) BridgeBase(_rollupProcessor) {
         rollupProcessor = _rollupProcessor;
+        // _mint(address(this), DUST) // for optimization
+    }
+
+    // function getPool(AztecTypes.AztecAsset calldata _inputAssetA, AztecTypes.AztecAsset calldata _outputAssetA) private returns(bool isInputCurveLpToken, uint poolPid, bool isPoolOpen, address curveLpToken, address convexToken, address curveRewards) {
+    //     uint poolLength = CONVEX_DEPOSIT.poolLength();
+
+    //      for (uint i=0; i < poolLength; i++) {
+    //         (address curveLpToken, address convexToken,, address curveRewards,, bool isPoolShut) = CONVEX_DEPOSIT.poolInfo(i);
+    //         if (curveLpToken == _inputAssetA.erc20Address && convexToken == _outputAssetA.erc20Address) {
+    //             return (true, i, !isPoolShut, curveLpToken, convexToken, curveRewards);
+    //         } else if (curveLpToken == _outputAssetA.erc20Address && convexToken == _inputAssetA.erc20Address) {
+    //             return (false, i, !isPoolShut, curveLpToken, convexToken, curveRewards);
+    //         } else {
+    //             revert invalidInputOutputAssets();
+    //         }
+    //     }
+    // }
+
+    function fillPools(uint currentPoolLength) public {
+        for (uint i=0; i < currentPoolLength; i++) {
+            (address curveLpToken, address convexToken,, address curveRewards,,) = CONVEX_DEPOSIT.poolInfo(i);
+            pools[curveLpToken] = PoolInfo(i, curveLpToken, convexToken, curveRewards, true);
+        }
     }
 
     // receive() external payable {} // now needed but I think it should be anywhere
-    
     /**
-    * @notice Stake Curve LP Token or withdraw Convex Token and exchange it back for Curve LP Tokens
-    * @notice To be able to stake a token, you'll already need to own Curve LP Token from one of its pools.
-    * This token is then placed into liquidity gauge by pool specific stash contract by Curve (or Convex? Unsure now). 
+    * @notice Staking and unstaking of Curve LP Tokens via Convex Finance in a form of Convex Tokens.
+    * @notice To be able to stake a token, you already need to own Curve LP Token from one of its pools.
+    * Staking: Curve LP Token is then placed into liquidity gauge by pool specific staker contract. 
     * New Convex Token is minted and then staked/deposited into rewardsContract.
-    * Withdrawing of stashed Convex Token goes in reversed order. First, the minted Convex Tokens will get burned.
-    * Curve LP Token will be after some more transfers credited back to RollUpProcessor.
+    * Withdrawing of Curve LP Tokens goes in reversed order. First, the minted Convex Tokens will get burned.
+    * If there is enough Curve LP tokens, tokens will be credited to the bridge. If this is not the case, 
+    * the necessary rest of Curve LP Token amount will be withdrawn from pool's liquidity gauge contract 
+    * and subsequently credited to the bridge and back to RollUpProcessor.
     * @notice Converting rate between Curve LP Token and corresponding Convex Token is 1:1
     * @param _inputAssetA Curve LP Token or Convex Token
     * @param _outputAssetA Convex Token or Curve LP Token
@@ -84,54 +129,52 @@ contract ConvexStakingBridge is BridgeBase {
     onlyRollup 
     returns(
         uint outputValueA,
-        uint isStakingSuccessful, 
+        uint, 
         bool // tady je vetsinou nejakej isAsync
     ) {
         if (_totalInputValue == 0) {
             revert invalidTotalInputValue();
         }
-        bool isInputCurveLpToken; // if input not Curve LP Token than it is Convex Token
 
-        uint poolLength = CONVEX_DEPOSIT.poolLength();
-        address _curveLpToken;
-        address _convexToken;
-        // address _curveRewards; // stack too deep, try removing local variables
-        for (uint i=0; i < poolLength; i++) {
-            (_curveLpToken, _convexToken,,,,) = CONVEX_DEPOSIT.poolInfo(i);
-            if (_curveLpToken == _inputAssetA.erc20Address && _convexToken == _outputAssetA.erc20Address) {
-                isInputCurveLpToken = true;
-                poolPid = i;
-                isMatchingPoolFound = true;
-                break;
-            }
-            if (_curveLpToken == _outputAssetA.erc20Address && _convexToken == _inputAssetA.erc20Address) {
-                isInputCurveLpToken = false; // could be removed, bool false by default
-                poolPid = i;
-                isMatchingPoolFound = true;
-                break;
-            }
+        uint currentPoolLength = CONVEX_DEPOSIT.poolLength();
+        if (lastPoolLength != currentPoolLength) {
+            fillPools(currentPoolLength);
+            lastPoolLength = currentPoolLength;
+        }
+        
+
+        PoolInfo memory selectedPool;
+        bool isInputCurveLpToken;
+
+        if (pools[_inputAssetA.erc20Address].exists) { // pak se jedna o Curve LP Token na vstupu =>
+            isInputCurveLpToken = true;
+            selectedPool = pools[_inputAssetA.erc20Address];
+        } else if (pools[_outputAssetA.erc20Address].exists) { // convex token na vstupu
+            selectedPool = pools[_outputAssetA.erc20Address];
+        } else {
+            revert invalidInputOutputAssets();
         }
 
-        require(isMatchingPoolFound, "No matching Curve pool found");
+        CURVE_LP_TOKEN = ICurveLpToken(selectedPool.curveLpToken);
+        CONVEX_TOKEN = IConvexToken(selectedPool.convexToken);
+        CURVE_REWARDS = ICurveRewards(selectedPool.curveRewards);
+        (,,,,,bool isPoolShutDown) = CONVEX_DEPOSIT.poolInfo(selectedPool.poolPid);
 
-        CURVE_LP_TOKEN = ICurveLpToken(_curveLpToken);
-        CONVEX_TOKEN = IConvexToken(_convexToken); // not used
-        // CURVE_REWARDS = ICurveRewards(_curveRewards);
+        if (isPoolShutDown) {
+            revert poolIsClosed(); // aby se nestalo, ze ten pool bude zavrenej a ja z nej chci alespon vybrat tokeny, ale nemuzu, mozna neni muj ukol though
+        }
 
-        // approvals
-        // CURVE_LP_TOKEN.approve(address(this), _totalInputValue);
-        CURVE_LP_TOKEN.approve(address(CONVEX_DEPOSIT), _totalInputValue);
-
-         // Approve rollup processor to take input value of input asset
-        // IERC20(_outputAssetA.erc20Address).approve(ROLLUP_PROCESSOR, _totalInputValue);
-
-
+        // staking
         if (isInputCurveLpToken) {
+            // approval
+            CURVE_LP_TOKEN.approve(address(CONVEX_DEPOSIT), _totalInputValue); // this should only be allowed for depositing
+            _approve(address(this), rollupProcessor, _totalInputValue);
+
             uint startCurveRewards = CURVE_REWARDS.balanceOf(address(this));
 
-            bool isStakingSuccessful = CONVEX_DEPOSIT.deposit(poolPid, _totalInputValue, true);
+            bool isStakingSuccessful = CONVEX_DEPOSIT.deposit(selectedPool.poolPid, _totalInputValue, true);
             
-            emit StakingResult(isStakingSuccessful);
+            emit StakingResult(isStakingSuccessful); // asi neni treba
 
             if(!isStakingSuccessful) {
                 revert stakingUnsuccessful();
@@ -139,21 +182,52 @@ contract ConvexStakingBridge is BridgeBase {
 
             uint endCurveRewards = CURVE_REWARDS.balanceOf(address(this)); 
 
-            // outputValueA = 10;
+            outputValueA = (endCurveRewards - startCurveRewards);
 
-            outputValueA = (endCurveRewards - startCurveRewards); // maybe devide by precision 
-        } else {
-            uint startCurveLpTokenAmount = CURVE_LP_TOKEN.balanceOf(address(this));
+            _mint(address(this), outputValueA);
+        } else { //withdrawing (unstaking)
 
-            bool isWithdrawalSuccessful = CONVEX_DEPOSIT.withdraw(poolPid, _totalInputValue);
+            // bridge should have tokens because I mocked it
+            emit BridgeTokenAmount(balanceOf(address(this)));
+
+            // approvals
+            CURVE_LP_TOKEN.approve(rollupProcessor, _totalInputValue);
+            _approve(address(this), rollupProcessor, _totalInputValue);
+
+            uint startCurveLpTokens = CURVE_LP_TOKEN.balanceOf(address(this));
+
+            bool isWithdrawalSuccessful = CONVEX_DEPOSIT.withdraw(selectedPool.poolPid, _totalInputValue);
 
             if(!isWithdrawalSuccessful) {
                 revert withdrawalUnsuccessful();
             }
 
-            uint endCurveLpTokenAmount = CURVE_LP_TOKEN.balanceOf(address(this));
+            uint endCurveLpTokens = CURVE_LP_TOKEN.balanceOf(address(this));
 
-            outputValueA = (endCurveLpTokenAmount - startCurveLpTokenAmount); // maybe devide by precision
+            outputValueA = (endCurveLpTokens - startCurveLpTokens);
+
+            emit BridgeTokenAmount(balanceOf(address(this)));
+            emit BridgeTokenAmount(outputValueA);
+            emit BridgeTokenAmount(totalSupply());
+
+
+            _burn(address(this), outputValueA); // no balance yet
         }
+
+        // bool isInputCurveLpToken; // if input not Curve LP Token than it is Convex Token
+
+
+        // (isInputCurveLpToken, poolPid, isPoolOpen, curveLpToken, convexToken, curveRewards) = getPool(_inputAssetA, _outputAssetA);
+
+        // CURVE_LP_TOKEN = ICurveLpToken(curveLpToken);
+        // CONVEX_TOKEN = IConvexToken(convexToken); // not used
+        // CURVE_REWARDS = ICurveRewards(curveRewards);
+
+        // approvals
+        // CURVE_LP_TOKEN.approve(address(this), _totalInputValue);
+
+         // Approve rollup processor to take input value of input asset
+        // IERC20(_outputAssetA.erc20Address).approve(ROLLUP_PROCESSOR, _totalInputValue);
+
     }
 }
--- src/test/bridges/convex/ConvexStakingBridge.t.sol
+++ src/test/bridges/convex/ConvexStakingBridge.t.sol
@@ -5,7 +5,6 @@ pragma solidity >=0.8.4;
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 import {BridgeTestBase} from "./../../aztec/base/BridgeTestBase.sol";
-
 import {ConvexStakingBridge} from "../../../bridges/convex/ConvexStakingBridge.sol";
 import {AztecTypes} from "../../../aztec/libraries/AztecTypes.sol";
 import {ErrorLib} from "../../../bridges/base/ErrorLib.sol";
@@ -15,7 +14,11 @@ contract ConvexStakingBridgeTest is BridgeTestBase {
     // ICurveLpToken private constant CURVE_LP_TOKEN = ICurveLpToken(0xe7A3b38c39F97E977723bd1239C3470702568e7B);
     address private constant CURVE_LP_TOKEN = 0xe7A3b38c39F97E977723bd1239C3470702568e7B;
     address private constant CONVEX_TOKEN = 0xbE665430e4C439aF6C92ED861939E60A963C6d0c;
+    address private constant BOOSTER = 0xF403C135812408BFbE8713b5A23a04b3D48AAE31;
     address private constant STAKER = 0x989AEb4d175e16225E39E87d0D97A3360524AD80;
+    address private constant GAUGE = 0x9f57569EaA61d427dEEebac8D9546A745160391C;
+    address private constant STASH = 0x8c8A5557F5ca466a93419042cdb407686545b1a8;
+    address private constant CRV_REWARDS = 0x14F02f3b47B407A7a0cdb9292AA077Ce9E124803;
 
     address private rollupProcessor;
 
@@ -27,27 +30,23 @@ contract ConvexStakingBridgeTest is BridgeTestBase {
         rollupProcessor = address(this);
         bridge = new ConvexStakingBridge(rollupProcessor);
 
-        vm.label(CURVE_LP_TOKEN, "Curve LP Token Contract Address");
-        vm.label(CONVEX_TOKEN, "Convex Token Contract Address");
+        vm.label(CURVE_LP_TOKEN, "Curve LP Token Contract");
+        vm.label(CONVEX_TOKEN, "Convex Token Contract");
         vm.label(STAKER, "Staker Contract Address");
-        vm.label(address(this), "Test Contract Address");
-        vm.label(address(bridge), "Bridge Contract Address");
-        vm.label(address(msg.sender), "Msg Sender address");
-        vm.label(address(0xF403C135812408BFbE8713b5A23a04b3D48AAE31), "Booster");
-        vm.label(address(0x14F02f3b47B407A7a0cdb9292AA077Ce9E124803), "CrvRewards Contract Address");
-        vm.label(address(0x8c8A5557F5ca466a93419042cdb407686545b1a8), "Stash Contract Address");
-        vm.label(address(0x9f57569EaA61d427dEEebac8D9546A745160391C), "Gauge Contract Address");
+        vm.label(address(this), "Test Contract");
+        vm.label(address(bridge), "Bridge");
+        vm.label(address(msg.sender), "Test Contract Msg Sender");
+        vm.label(BOOSTER, "Booster");
+        vm.label(CRV_REWARDS, "CrvRewards Contract");
+        vm.label(STASH, "Stash Contract");
+        vm.label(GAUGE, "Gauge Contract");
     }
 
     function testBridge() public {
         assertTrue(true);
     }
 
-    function testStakingLpTokens(uint96 depositAmount) public {
-        // if (depositAmount == 0) {
-        //     return;
-        // }
-
+    function testStakeLpTokens(uint96 depositAmount) public {
         AztecTypes.AztecAsset memory inputAssetA = AztecTypes.AztecAsset({
             id: 1,
             erc20Address: CURVE_LP_TOKEN,
@@ -56,17 +55,16 @@ contract ConvexStakingBridgeTest is BridgeTestBase {
 
         AztecTypes.AztecAsset memory outputAssetA = AztecTypes.AztecAsset({
             id: 100,
-            erc20Address: CONVEX_TOKEN,
+            erc20Address: address(bridge),
             assetType: AztecTypes.AztecAssetType.ERC20
         });
 
-        // deal(CURVE_LP_TOKEN, address(this), 10);      
-        deal(CURVE_LP_TOKEN, address(bridge), depositAmount);   // misto 10 udelat fuzz s uint96 _depositAmount   
+        deal(CURVE_LP_TOKEN, address(bridge), depositAmount);
 
         if (depositAmount == 0) {
             vm.expectRevert(invalidTotalInputValue.selector);
         }
-    
+   
         (uint outputValueA, uint outputValueB, bool isAsync) = bridge.convert(
             inputAssetA,
             emptyAsset,
@@ -77,77 +75,109 @@ contract ConvexStakingBridgeTest is BridgeTestBase {
             0,
             address(11)
         );
-
-        // assertTrue(stakingSuccessful);
-
-        // assertEq(stakingSuccessful, 1);
-
         assertEq(outputValueA, depositAmount);
-        assertEq(outputValueB, 0, "Output value B is not 0.");
+        assertEq(outputValueB, 0, "Output value B is not 0."); // I am not really returning these two, so it actually returns a default..
         assertTrue(!isAsync, "Bridge is in async mode which it shouldn't");
+
+        assertEq(bridge.balanceOf(address(bridge)), outputValueA);
+        IERC20(outputAssetA.erc20Address).transferFrom(address(bridge), rollupProcessor, outputValueA);
+        assertEq(bridge.balanceOf(address(bridge)), 0);
+        assertEq(bridge.balanceOf(rollupProcessor), outputValueA);
     }
 
-    // function testStakingLpTokensZero() public {
-    //     AztecTypes.AztecAsset memory inputAssetA = AztecTypes.AztecAsset({
-    //         id: 1,
-    //         erc20Address: CURVE_LP_TOKEN,
-    //         assetType: AztecTypes.AztecAssetType.ERC20
-    //     });
+    function testWithdrawLpTokens(uint64 withdrawalAmount) public {
+        // deposit LpTokens first so the totalSupply of minted tokens match (no other workaround)
+        _deposit(withdrawalAmount);
 
-    //     AztecTypes.AztecAsset memory outputAssetA = AztecTypes.AztecAsset({
-    //         id: 100,
-    //         erc20Address: CONVEX_TOKEN,
-    //         assetType: AztecTypes.AztecAssetType.ERC20
-    //     });
+        AztecTypes.AztecAsset memory inputAssetA = AztecTypes.AztecAsset({
+            id: 100,
+            erc20Address: address(bridge),
+            assetType: AztecTypes.AztecAssetType.ERC20
+        });
 
-    //     // deal(CURVE_LP_TOKEN, address(this), 10);      
-    //     deal(CURVE_LP_TOKEN, address(bridge), 0);   // misto 10 udelat fuzz s uint96 _depositAmount   
+        AztecTypes.AztecAsset memory outputAssetA = AztecTypes.AztecAsset({
+            id: 1,
+            erc20Address: CURVE_LP_TOKEN,
+            assetType: AztecTypes.AztecAssetType.ERC20
+        });
 
-    //     vm.expectRevert(invalidTotalInputValue.selector);
-    //     (uint outputValueA, uint outputValueB, bool isAsync) = bridge.convert(
-    //         inputAssetA,
-    //         emptyAsset,
-    //         outputAssetA,
-    //         emptyAsset,
-    //         0,
-    //         0,
-    //         0,
-    //         address(11)
-    //     );
+        // deal(inputAssetA.erc20Address, address(bridge), withdrawalAmount);
+        deal(CONVEX_TOKEN, address(bridge), withdrawalAmount);
 
-    //     // assertTrue(stakingSuccessful);
+        // transfer CSB Token from RollUpProcessor to the bridge
+        IERC20(inputAssetA.erc20Address).transfer(address(bridge), withdrawalAmount);
+        
+        if (withdrawalAmount == 0) {
+            vm.expectRevert(invalidTotalInputValue.selector);
+        }
 
-    //     // assertEq(stakingSuccessful, 1);
+        (uint outputValueA, uint outputValueB, bool isAsync) = bridge.convert(
+            inputAssetA,
+            emptyAsset,
+            outputAssetA,
+            emptyAsset,
+            withdrawalAmount,
+            0,
+            0,
+            address(11)
+        );
+        assertEq(outputValueA, withdrawalAmount);
+        assertEq(outputValueB, 0, "Output value B is not 0."); // I am not really returning these two, so it actually returns a default..
+        assertTrue(!isAsync, "Bridge is in async mode which it shouldn't");
 
-    //     assertEq(outputValueA, 0);
-    //     assertEq(outputValueB, 0, "Output value B is not 0.");
-    //     assertTrue(!isAsync, "Bridge is in async mode which it shouldn't");
-    // }
+        assertEq(IERC20(CURVE_LP_TOKEN).balanceOf(address(bridge)), withdrawalAmount);
+        IERC20(CURVE_LP_TOKEN).transferFrom(address(bridge), rollupProcessor, withdrawalAmount);
+        assertEq(IERC20(CURVE_LP_TOKEN).balanceOf(rollupProcessor), withdrawalAmount);
 
-    function testWithdrawingLpTokens() public {
+        assertEq(IERC20(address(bridge)).balanceOf(address(bridge)), 0);
+        assertEq(IERC20(CURVE_LP_TOKEN).balanceOf(rollupProcessor), 0);
+    }
+
+    function _deposit(uint depositAmount) internal {
+        // 1. mock number of Curve LP Tokens to stake
+        deal(CURVE_LP_TOKEN, address(bridge), depositAmount);
+
         AztecTypes.AztecAsset memory inputAssetA = AztecTypes.AztecAsset({
             id: 1,
-            erc20Address: CONVEX_TOKEN,
+            erc20Address: CURVE_LP_TOKEN,
             assetType: AztecTypes.AztecAssetType.ERC20
         });
 
         AztecTypes.AztecAsset memory outputAssetA = AztecTypes.AztecAsset({
             id: 100,
-            erc20Address: CURVE_LP_TOKEN,
+            erc20Address: address(bridge),
             assetType: AztecTypes.AztecAssetType.ERC20
         });
 
-        // deal(address(CURVE_LP_TOKEN), address(this), 10);
-
-        bridge.convert(
+        if (depositAmount == 0) {
+            vm.expectRevert(invalidTotalInputValue.selector);
+        }
+   
+        // 2. stake tokens
+        (uint outputValueA, uint outputValueB, bool isAsync) = bridge.convert(
             inputAssetA,
             emptyAsset,
             outputAssetA,
             emptyAsset,
-            2,
-            12345,
+            depositAmount,
+            0,
             0,
             address(11)
         );
+        assertEq(outputValueA, depositAmount);
+        assertEq(outputValueB, 0, "Output value B is not 0."); // I am not really returning these two, so it actually returns a default..
+        assertTrue(!isAsync, "Bridge is in async mode which it shouldn't");
+
+        assertEq(bridge.balanceOf(address(bridge)), outputValueA);
+
+        // 3. Check the total supply of SPB token is equal to the amount of LUSD deposited
+        assertEq(bridge.totalSupply(), depositAmount);
+
+        // 4. Transfer SPB back to RollupProcessor
+        IERC20(address(bridge)).transferFrom(address(bridge), rollupProcessor, outputValueA);
+
+        // 5. Check the SPB balance of rollupProcessor is equal to the amount of LUSD deposited
+        assertEq(outputValueA, depositAmount);
+        assertEq(bridge.balanceOf(rollupProcessor), depositAmount);
     }
 }
