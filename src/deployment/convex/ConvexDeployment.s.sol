285aeaff87f20ff38bb9e62382f51c73f492ac8e
Vojtech Kaiser <vojtechkaiser95@gmail.com>
2022-11-20 21:38:00 +0100

    deployment - possible ways to load RCT tokens by pool id or curve lp token

--- src/deployment/convex/ConvexDeployment.s.sol
+++ src/deployment/convex/ConvexDeployment.s.sol
@@ -7,14 +7,19 @@ import {ConvexStakingBridge} from "../../bridges/convex/ConvexStakingBridge.sol"
 import {IConvexBooster} from "../../interfaces/convex/IConvexBooster.sol";
 
 contract ConvexStakingBridgeDeployment is BaseDeployment {
-    IConvexBooster public constant BOOSTER = IConvexBooster(0xF403C135812408BFbE8713b5A23a04b3D48AAE31);
-    ConvexStakingBridge public bridge;
+    IConvexBooster private constant BOOSTER = IConvexBooster(0xF403C135812408BFbE8713b5A23a04b3D48AAE31);
+    ConvexStakingBridge private bridge;
+
+    uint256[] private poolIds = [69]; // e.g. [10, 110] for pools 10 and 110
+    address[] private curveLpTokens; // e.g. [0xe7A3b38c39F97E977723bd1239C3470702568e7B] for pool 112
+
+    event ShowBridgeAddress(address bridge);
 
     function deployAndList() public {
         deploy();
         uint256 addressId = listBridge(address(bridge), 1000000);
 
-        // list every single pool curve lp token
+        // list all pool Curve LP tokens
         _listAllAssets();
 
         emit log_named_uint("Convex staking bridge address id", addressId);
@@ -28,12 +33,50 @@ contract ConvexStakingBridgeDeployment is BaseDeployment {
         emit log_named_address("Convex staking bridge deployed to", address(bridge));
     }
 
+    function loadPools() public {
+        // Insert pool ids in the poolIds variable at the top of the contract to have the pools loaded and their RCT tokens listed
+        bridge = new ConvexStakingBridge(ROLLUP_PROCESSOR);
+        emit ShowBridgeAddress(address(bridge));
+
+        for (uint256 i = 0; i < poolIds.length; i++) {
+            bridge.loadPool(poolIds[i]);
+            _listToken(poolIds[i]);
+        }
+    }
+
+
+    function listTokensByPoolId() public {
+        // Insert pool ids in the poolIds variable at the top of the contract to have the RCT tokens listed
+        bridge = new ConvexStakingBridge(ROLLUP_PROCESSOR);
+
+        for (uint256 i = 0; i < poolIds.length; i++) {
+            (address curveLpToken, , , , , ) = BOOSTER.poolInfo(poolIds[i]);
+            address rctToken = bridge.deployedClones(curveLpToken);
+            listAsset(rctToken, 100000);
+        }
+    }
+
+    function listTokensByCurveLpToken() public {
+        // insert Curve LP tokens in the curveLpTokens variable at the top of the contract to have the matching RCT tokens listed
+        bridge = new ConvexStakingBridge(ROLLUP_PROCESSOR);
+
+        for (uint256 i = 0; i < curveLpTokens.length; i++) {
+            address rctToken = bridge.deployedClones(curveLpTokens[i]);
+            listAsset(rctToken, 100000);
+        }
+    }
+
     function _listAllAssets() internal {
         uint256 poolLength = BOOSTER.poolLength();
-        for (uint256 i = 0; i < poolLength; i++) {
-            (address curveLpToken, , , , , ) = BOOSTER.poolInfo(i);
+        for (uint256 pid = 0; pid < poolLength; pid++) {
+            (address curveLpToken, , , , , ) = BOOSTER.poolInfo(pid);
             listAsset(curveLpToken, 100000);
-            listAsset(bridge.deployedClones(curveLpToken), 100000);
         }
+    }
+
+    function _listToken(uint256 _poolId) internal {
+        (address curveLpToken, , , , , ) = BOOSTER.poolInfo(_poolId);
+        address rctToken = bridge.deployedClones(curveLpToken);
+        listAsset(rctToken, 100000);
     }
 }
